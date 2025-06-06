ec26d16ebc8e71eb09ffcec2ff10c1943762a825
veena <veenajithesh66@gmail.com>
2025-06-06 16:29:10 +0530

    Initial commit

---
+++ app1/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def store(request):
+    return render(request, 'store.html')    
+
+def branch(request):
+    return render(request, 'branch.html')
+
+def contact(request):
+    return render(request, 'contact.html')
